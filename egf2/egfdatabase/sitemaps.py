a2fb4fb709d277bfafae045fe02bc39fa2c82abc
abraner <53873357+abraner@users.noreply.github.com>
2021-11-08 18:44:19 -0500

    Update sitemaps.py

--- egf2/egfdatabase/sitemaps.py
+++ egf2/egfdatabase/sitemaps.py
@@ -1,1 +1,20 @@
+from django.contrib import sitemaps
+from django.urls import reverse
 
+class StaticViewsSitemap(sitemaps.Sitemap):
+
+    changefreq = "daily"
+    priority = 0.5
+
+    def items(self):
+
+        return [
+            'egfdatabase:index',
+            'egfdatabase:about',
+            'egfdatabase:evaluation',
+            'egfdatabase:services',
+            'egfdatabase:contact',
+        ]
+
+    def location(self, item):
+        return reverse(item)
